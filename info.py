0c6f14c81dd97291dc5aa27d47c570f13b9801fc
Tvshows122 <126122240+Tvshows122@users.noreply.github.com>
2025-03-15 20:48:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -68,7 +68,7 @@ APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 # MongoDB Configuration
 # ============================
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://tvshows122:tvshows122@cluster0.gkpbj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_URI2 = environ.get('DATABASE_URI2', "")
+DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://tvshows122:tvshows122@cluster0.gkpbj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "rockybhai")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'files')
 
